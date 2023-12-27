cb4679b3d306bb267e53836ae0a33faec8a83f66
K-Ryo-ta <studyprograming51@gmail.com>
2023-12-27 11:42:30 +0900

    ABC334解き直し

--- ABC334/B.py
+++ ABC334/B.py
@@ -1,43 +1,4 @@
 A,M,L,R = map(int, input().split())
-counttree = 0
-if A > L and A < R:
-    i=0
-    while True:
-        i += 1
-        if A + i * M > R:
-            break
-        else:
-            counttree += 1
-        
-    i=0
-    while True:
-        i += 1
-        if A - i * M < L:
-            break
-        else:
-            counttree += 1
-    print(counttree+1)
-
-elif A < L and A < R:
-    i=0
-    while True:
-        i += 1
-        if A + i * M > R:
-            break
-        elif L  == R:
-            break
-        else:
-            counttree += 1
-    print(counttree)
-
-# elif A > L and A > R:      
-#     i=0
-#     while True:
-#         i += 1
-#         if A - i * M < L:
-#             break
-#         elif L == R:
-#             break
-#         else:
-#             counttree += 1
-#     print(counttree)
+L -= A
+R -= A
+print(R//M - (L-1)//M)
